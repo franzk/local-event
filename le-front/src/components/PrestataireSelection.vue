ae783d7be72364b105cd49f0694c33f85a37f1d7
François Koehler <francois.koehler@lde.fr>
2024-12-06 23:58:09 +0100

    home page WIP

---
+++ le-front/src/components/PrestataireSelection.vue
@@ -0,0 +1,12 @@
+<template>
+	<section id="#prestataire-selection"></section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+#prestataire-selection {
+	flex: 1;
+	border-top: 1px solid black;
+}
+</style>
